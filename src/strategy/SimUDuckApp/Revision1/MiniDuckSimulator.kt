f092a815eabd8f926cad9519fafc3e2d9c21a015
kwanulee <kwlee@hansung.ac.kr>
2024-02-26 17:35:10 +0900

    Update MiniDuckSimulator.kt

--- src/strategy/SimUDuckApp/Revision1/MiniDuckSimulator.kt
+++ src/strategy/SimUDuckApp/Revision1/MiniDuckSimulator.kt
@@ -1,21 +1,20 @@
 package strategy.SimUDuckApp.Revision1
 
-object MiniDuckSimulator {
-    @JvmStatic
-    fun main() {
-        val ducks = ArrayList<Duck>()
 
-        ducks.add(MallardDuck())
-        ducks.add(RedheadDuck())
-        ducks.add(RubberDuck())
-        ducks.add(DecoyDuck())
+fun main() {
+    val ducks = ArrayList<Duck>()
 
-        for (d in ducks) {
-            d.display()
-            d.swim()
-            d.quack()
-            d.fly()
-            println()
-        }
+    ducks.add(MallardDuck())
+    ducks.add(RedheadDuck())
+    ducks.add(RubberDuck())
+    ducks.add(DecoyDuck())
+
+    for (d in ducks) {
+        d.display()
+        d.swim()
+        d.quack()
+        d.fly()
+        println()
     }
 }
+
